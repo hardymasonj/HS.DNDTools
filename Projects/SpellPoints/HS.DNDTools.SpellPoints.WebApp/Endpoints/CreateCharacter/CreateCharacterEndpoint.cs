e1bc6b5543b703f7029e0d70227f239327a9c732
Mason Hardy <hardymasonj@gmail.com>
2022-03-15 22:46:52 -0500

    Added CreateCharacter Endpoint

---
+++ Projects/SpellPoints/HS.DNDTools.SpellPoints.WebApp/Endpoints/CreateCharacter/CreateCharacterEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using HS.DNDTools.SpellPoints.Application.Actions;
+using HS.DNDTools.SpellPoints.WebApp.Models;
+
+namespace HS.DNDTools.SpellPoints.WebApp.Endpoints.CreateCharacter
+{
+    public class CreateCharacterEndpoint : Endpoint<CreateCharacterRequest, CreateCharacterResponse>
+    {
+        public override void Configure()
+        {
+            this.Get("api/v1/new-character/{Name}/{Level}");
+            this.AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(CreateCharacterRequest req, CancellationToken ct)
+        {
+            var response = new CreateCharacterResponse();
+            response.Character = new CharacterModel();
+            response.Character.Name = req.Name;
+            response.Character.MaxSpellPoints = (new MaxSpellPoints()).CalculateForLevel(req.Level);
+            response.Character.CurrentSpellPoints = response.Character.MaxSpellPoints;
+            response.Character.Level = req.Level;
+            await SendAsync(response);
+        }
+    }
+}
